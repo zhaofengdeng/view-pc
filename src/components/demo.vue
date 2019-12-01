1a791858c0343bc2b037d66cfdcbbecfa197346a
zhaofengdeng <18201075336@139.com>
2019-12-01 12:41:07 +0800

    project init

---
+++ src/components/demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <van-button type="primary" @click="test">弹出框</van-button>
+</template>
+  <script>
+import { Dialog } from "vant";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    test() {
+      Dialog.alert({
+        title: "标题",
+        message: "弹窗内容"
+      }).then(() => {
+        // on close
+      });
+    }
+  }
+};
+</script>
