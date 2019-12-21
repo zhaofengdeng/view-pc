9c78653ba20a7291359b1cebff2682c174ea95c6
zhaofengdeng <18201075336@139.com>
2019-12-21 12:38:02 +0800

    路由变更增加日志监测

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,28 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {};
+  },
   watch: {
     $route: {
       handler: function(val, oldVal) {
         this.closeAllErrorMsg();
+        var curPath = val.path;
+        console.log(val);
+        var lastPath = oldVal.path;
+        var model = {
+          path: curPath,
+          lastPath: lastPath,
+          query: JSON.stringify(val.query),
+          id: localStorage.getItem("lastPageLogId")
+        };
+
+        //记录日志
+        this.post("/log/page/end", model).then(res => {});
+        this.post("/log/page/begin", model).then(res => {
+          localStorage.setItem("lastPageLogId", res.id);
+        });
       },
       // 深度观察监听
       deep: true
